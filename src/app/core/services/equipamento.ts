7cf1886f574d03b552350c750f70d57ed5ee2172
monique <moniquesnmonteiro@gmail.com>
2026-04-28 21:48:45 -0400

    fix: ajusta validacao de perfil no update de Equipamentos

--- src/app/core/services/equipamento.ts
+++ src/app/core/services/equipamento.ts
@@ -9,7 +9,7 @@ import { Equipamento } from '../models/equipamento.model';
 export class EquipamentoService {
   private readonly API = 'http://localhost:3000/api/equipamentos';
   
-  private readonly token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjcsInBlcmZpbCI6MiwiaWF0IjoxNzc3MzQyNDE3LCJleHAiOjE3NzczNDMzMTd9.iPvFQwFzzAoC3dEwK0Ruw-Wc-ai2OfNdryAXQfzscxg'; 
+  private readonly token = 'cole_seu_token_aqui'; 
 
   constructor(private http: HttpClient) { }
 
@@ -27,7 +27,7 @@ export class EquipamentoService {
     return this.http.get<Equipamento[]>(this.API, { headers: this.getHeaders() });
   }
 
-  update(id: string, equipamento: Partial<Equipamento>): Observable<Equipamento> {
-    return this.http.put<Equipamento>(`${this.API}/${id}`, equipamento);
-  }
+update(id: string, equipamento: Partial<Equipamento>): Observable<Equipamento> {
+  return this.http.put<Equipamento>(`${this.API}/${id}`, equipamento, { headers: this.getHeaders() });
+}
 }
